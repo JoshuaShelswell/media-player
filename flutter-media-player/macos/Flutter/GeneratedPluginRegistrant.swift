3903c0f61341e5e1f197a764bd5cc034542d84a3
paleaura01 <99763057+paleaura01@users.noreply.github.com>
2025-04-18 17:26:44 -0400

    1
    1

--- flutter-media-player/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter-media-player/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import desktop_drop
+import file_picker
 import path_provider_foundation
 import screen_retriever_macos
 import shared_preferences_foundation
@@ -13,6 +14,7 @@ import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DesktopDropPlugin.register(with: registry.registrar(forPlugin: "DesktopDropPlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
--- flutter-media-player/windows/runner/main.cpp
+++ flutter-media-player/windows/runner/main.cpp
@@ -1,14 +1,15 @@
-#include <flutter/dart_project.h>
-#include <flutter/flutter_view_controller.h>
-#include <shobjidl.h> 
+#include <windows.h>
+#include <shobjidl.h>   // for SetCurrentProcessExplicitAppUserModelID
 #include "flutter_window.h"
 #include "utils.h"
 
-int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
-                      _In_ wchar_t *command_line, _In_ int show_command) {
+int APIENTRY wWinMain(_In_ HINSTANCE instance,
+                      _In_opt_ HINSTANCE prev,
+                      _In_ wchar_t* command_line,
+                      _In_ int show_command) {
+  // Tell Windows which AppID to use (so the taskbar jumplist reads “Amped”)
+  SetCurrentProcessExplicitAppUserModelID(L"com.paleaura01.Amped");
 
-  const wchar_t kAppID[] = L"com.yourcompany.Amped";
-  SetCurrentProcessExplicitAppUserModelID(kAppID);
   // Attach to console when present (e.g., 'flutter run') or create a
   // new console when running with a debugger.
   if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
@@ -20,18 +21,18 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   ::CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
 
   flutter::DartProject project(L"data");
-
   std::vector<std::string> command_line_arguments =
       GetCommandLineArguments();
-
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
+
   if (!window.Create(L"Amped", origin, size)) {
     return EXIT_FAILURE;
   }
+
   window.SetQuitOnClose(true);
 
   ::MSG msg;
